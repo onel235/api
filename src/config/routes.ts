cdd8561db6192ac9ba89c3adb58fd8eb321f2ea9
Bruno <stacheskibruno@gmail.com>
2021-04-03 15:30:17 -0300

    Added basic login route (without JWT for the time being).

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
+import { LoginController } from '../controllers/LoginController';
 
 const routes = Router();
 const user = new UserController();
+const login = new LoginController();
 
 routes.post('/users/create', user.create);
+routes.post('/login', login.create);
 
 export default routes;
